d5a782c650ae34db7058a906a1848cc86852d1c1
karingi <emmanuelkaringi@gmail.com>
2022-07-26 20:15:21 +0300

    added a new file with pylint recommended changes

---
+++ app_modified.py
@@ -0,0 +1,63 @@
+"""A quiz app based on the show: The Big Bang Theory"""
+import random
+from string import ascii_lowercase
+
+NUM_QUESTIONS_PER_QUIZ = 5
+QUESTIONS = {
+    "How old was Sheldon when he got his first P.h.d?": [
+        "16",
+        "18",
+        "20",
+        "22",
+    ],
+    "What’s Amy’s job": [
+        "Neurobiologist",
+        "Marine biologist",
+        "Forensic biologist",
+        "Microbiologist",
+    ],
+    "Which couple gets married first?": [
+        "Bernadette and Howard",
+        "Penny and Leonard",
+        "Amy and Sheldon",
+        "Emily and raj",
+    ],
+    "What is Leonard primary health concern?": [
+        "He is lactose intolerant",
+        "He is allergic to peanuts",
+        "He has to eat gluten free",
+        "He has diabetes",
+    ],
+    "Who does Howard meet while serving Thanksgiving dinner at a shelter?": [
+        "Elon Musk",
+        "Jeff Bezos",
+        "Bill Gates",
+        "Steve Jobs",
+    ]
+}
+
+num_questions = min(NUM_QUESTIONS_PER_QUIZ, len(QUESTIONS))
+questions = random.sample(list(QUESTIONS.items()), k=num_questions)
+
+NUM_CORRECT = 0
+for num, (question, alternatives) in enumerate(questions, start=1):
+    print(f"\nQuestion {num}:")
+    print(f"{question}?")
+    correct_answer = alternatives[0]
+    labeled_alternatives = dict(
+        zip(ascii_lowercase, random.sample(alternatives, k=len(alternatives)))
+    )
+    for label, alternative in labeled_alternatives.items():
+        print(f"  {label}) {alternative}")
+
+    while (answer_label := input("\nChoice? ")) not in labeled_alternatives:
+        print(f"Please answer one of {', '.join(labeled_alternatives)}")
+
+    answer = labeled_alternatives[answer_label]
+    if answer == correct_answer:
+        NUM_CORRECT += 1
+        print("⭐ Correct! ⭐")
+    else:
+        print(f"The answer is {correct_answer!r}, not {answer!r}")
+
+print(f"\nYou got {NUM_CORRECT} correct out of {num} questions")
